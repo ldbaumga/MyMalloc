dcbdcb2f9b5b6c0eef39c5afec4f9d4fc9fdcf6c
Lucas Daniel Baumgartner <ldbaumga@escher03.cs.purdue.edu>
2021-08-31 10:17:10 -0400

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -227,7 +227,7 @@ static inline header * allocate_object(size_t raw_size) {
 
   //Searches throught the freelist to find the first block that is greater than
   //or equal to alloc_size and breaks
-  for (int i = index; i < N_LISTS; i++;) {
+  for (int i = index; i < N_LISTS; i++) {
     while (freelist->next != NULL) {
       freelist = freelist->next;
 
