f7407201eb068bb8ceb6d029910c792f017d58f6
Lucas Daniel Baumgartner <ldbaumga@escher03.cs.purdue.edu>
2021-08-31 10:11:28 -0400

    Commit

--- myMalloc.h
+++ myMalloc.h
@@ -12,7 +12,7 @@
 #endif
 
 #ifndef N_LISTS
- If not specified at compile time use the default number of free lists
+// If not specified at compile time use the default number of free lists
 #define N_LISTS 59
 #endif
 
