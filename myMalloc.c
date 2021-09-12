61de6fc23638614f3ea88e895df2547166d9940d
Lucas Daniel Baumgartner <ldbaumga@data.cs.purdue.edu>
2021-09-11 20:45:24 -0400

    Commit

--- myMalloc.c
+++ myMalloc.c
@@ -204,7 +204,7 @@ static inline int freelist_index(size_t size) {
  */
 static inline header * allocate_object(size_t raw_size) {
   // TODO implement allocation
-
+/*
   //Checks to see if raw_size is 0, returns null if true
   if (raw_size == 0) {
     return NULL;
@@ -275,7 +275,7 @@ static inline header * allocate_object(size_t raw_size) {
     //TODO when object is bigger than 512
     //return NULL;
   }
-
+*/
 }
 
 /**tab
